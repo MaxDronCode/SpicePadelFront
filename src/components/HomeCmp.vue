3bbd9d849f652b2f2b8ade31b45d1812547880bf
Max <Max>
2024-05-22 20:11:04 +0200

    Torneos working

--- src/components/HomeCmp.vue
+++ src/components/HomeCmp.vue
@@ -2,19 +2,19 @@
     <NavCmp/>
     <h1>HOME</h1>
     <TestApiCmp/>
-    <AccountCardCmp></AccountCardCmp>
+    <!-- <AccountCardCmp></AccountCardCmp> -->
 </template>
 
 <script>
 import NavCmp from './NavCmp.vue';
 import TestApiCmp from './TestApiCmp.vue';
-import AccountCardCmp from './AccountCardCmp.vue';
+// import AccountCardCmp from './AccountCardCmp.vue';
 export default {
     name: "HomeCmp",
     components: { 
         NavCmp,
         TestApiCmp,
-        AccountCardCmp,
+        // AccountCardCmp,
     }
 }
 </script>
