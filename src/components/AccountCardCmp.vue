7f5017114537d1fdcd8a796c1be9cc5eaca3f909
Martí <Mart>
2024-05-23 10:22:31 +0200

    changes on AccountCardCmp

--- src/components/AccountCardCmp.vue
+++ src/components/AccountCardCmp.vue
@@ -7,15 +7,11 @@
                     <img :src=logo alt="Logo">
                     <svg class="chip" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 256 256"><path fill="#d9d9d9" d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24M74.08 197.5a64 64 0 0 1 107.84 0a87.83 87.83 0 0 1-107.84 0M96 120a32 32 0 1 1 32 32a32 32 0 0 1-32-32m97.76 66.41a79.66 79.66 0 0 0-36.06-28.75a48 48 0 1 0-59.4 0a79.66 79.66 0 0 0-36.06 28.75a88 88 0 1 1 131.52 0"/></svg>
                     <p class="number">{{ dni }}</p>
-                    <p class="valid_thru">VALID THRU</p>
-                    <p class="date_8264">1 2 / 2 4</p>
+                    <p class="date_8264">{{ birthday }}</p>
                 </div>
                 <div class="flip-card-back">
                     <div class="strip"><p></p></div>
                     <div class="mstrip"><p>{{ usuMail }}</p></div>
-                    <div class="sstrip">
-                        <p class="code">{{ birthday }}</p>
-                    </div>
                 </div>
             </div>
         </div>
@@ -52,8 +48,6 @@ export default {
                     })
                 })
                 const data = await response.json()
-                console.log("nombre:")
-                console.log(data.user_name)
                 this.user_name = data.user_name
                 this.dni = data.dni
                 this.birthday = data.birthday
@@ -149,7 +143,7 @@ img {
 .mstrip {
   position: absolute;
   background-color: rgb(255, 255, 255);
-  width: 8em;
+  width: 13em;
   height: 0.8em;
   top: 5em;
   left: 0.8em;
--- src/components/HomeCmp.vue
+++ src/components/HomeCmp.vue
@@ -2,19 +2,18 @@
     <NavCmp/>
     <h1>HOME</h1>
     <TestApiCmp/>
-    <AccountCardCmp></AccountCardCmp>
+
 </template>
 
 <script>
 import NavCmp from './NavCmp.vue';
 import TestApiCmp from './TestApiCmp.vue';
-import AccountCardCmp from './AccountCardCmp.vue';
+
 export default {
     name: "HomeCmp",
     components: { 
         NavCmp,
         TestApiCmp,
-        AccountCardCmp,
     }
 }
 </script>
