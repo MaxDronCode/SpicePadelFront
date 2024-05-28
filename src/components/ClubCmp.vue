3d10ef1536c11866fc0c565b64855568d79e60c3
Max <Max>
2024-05-28 10:22:52 +0200

    footer mejoras

--- src/components/ClubCmp.vue
+++ src/components/ClubCmp.vue
@@ -2,7 +2,7 @@
     <NavCmp></NavCmp>
     <h1>CONOCENOS</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, earum. At tempora consequuntur obcaecati. Tenetur velit deleniti suscipit mollitia, optio corporis inventore earum delectus ex, laudantium officia facilis illum esse.</p>
-    <FooterCmp></FooterCmp>
+    <FooterCmp class="footer"></FooterCmp>
 </template>
 
 <script>
@@ -23,5 +23,8 @@ import FooterCmp from './FooterCmp.vue';
 </script>
 
 <style scoped>
-
+.footer{
+    position: absolute;
+    bottom: 0;
+}
 </style>
