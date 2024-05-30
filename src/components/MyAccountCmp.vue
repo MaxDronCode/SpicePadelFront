986254cb09983aa1a818d5b5073b18fb134abef0
Max <Max>
2024-05-30 09:20:27 +0200

    se cambian algunas rutas

--- src/components/MyAccountCmp.vue
+++ src/components/MyAccountCmp.vue
@@ -56,7 +56,7 @@ export default {
         async obtainName() {
             try {
                 const response = await fetch(
-                    "http://localhost/spicepadel_api/api/getName.php",
+                    "http://Test2php-env.eba-tdje93tb.us-east-1.elasticbeanstalk.com/spicepadel_api/api/getName.php",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
