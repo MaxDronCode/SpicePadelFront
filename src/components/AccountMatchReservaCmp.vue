60ef497af107ada30a4e6445435d8ddb2e9c5d26
Martí <Mart>
2024-05-30 12:49:42 +0200

    account matches

--- src/components/AccountMatchReservaCmp.vue
+++ src/components/AccountMatchReservaCmp.vue
@@ -10,7 +10,7 @@
             <path fill="#ffffff" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" clip-rule="evenodd" fill-rule="evenodd"></path>
           </g>
         </svg>
-        <p v-if="field_id==''">Campo de reserva: N/A</p> <p v-else>Campo de reserva: {{ field_id }}</p>
+        <p v-if="field_id==''">No hay campos reservados</p> <p v-else>Campo de reserva: {{ field_id }}</p>
       </li>
       <li class="list">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -20,7 +20,7 @@
             <path fill="#ffffff" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" clip-rule="evenodd" fill-rule="evenodd"></path>
           </g>
         </svg>
-        <p v-if="startHour==''">Hora de inicio: N/A</p> <p v-else>Hora de inicio: {{ startHour }}</p>
+        <p v-if="startHour==''">No hay hora de inicio</p> <p v-else>Hora de inicio: {{ startHour }}</p>
       </li>
       <li class="list">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -30,7 +30,7 @@
             <path fill="#ffffff" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" clip-rule="evenodd" fill-rule="evenodd"></path>
           </g>
         </svg>
-        <p v-if="end_hour==''">Hora de fin: N/A</p> <p v-else>Hora de fin: {{ end_hour }}</p>
+        <p v-if="end_hour==''">No hay hora de final</p> <p v-else>Hora de fin: {{ end_hour }}</p>
       </li>
       <li class="list">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -40,7 +40,7 @@
             <path fill="#ffffff" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" clip-rule="evenodd" fill-rule="evenodd"></path>
           </g>
         </svg>
-        <p v-if="date==''">Fecha: N/A</p> <p v-else>Fecha: {{ date }}</p>
+        <p v-if="date==''">No hay fecha</p> <p v-else>Fecha: {{ date }}</p>
       </li>
     </ul>
     <router-link to='/reserve' class="action">Reservar pista</router-link>
